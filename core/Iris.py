3fd352d9f4e4b6ffdfcc051b281e2085d540b5d9
IceCereal <ns_raghav@hotmail.com>
2019-07-07 10:41:25 +0530

    updated Iris.py

--- core/Iris.py
+++ core/Iris.py
@@ -1,49 +1,87 @@
-"""2018-May-24 09:55
-Maid-Sama
-Name given by:
-Alex Rothschild#6666
+"""
+	Name: Iris
+	Objective: A discord bot for administrative purposes
+	Date: 2019-Jul-06
+
+	Author: IceCereal
 """
 
 import discord
 from discord.ext import commands
-import asyncio
-import ast
 
-from os import listdir
-from os.path import isfile, join
+from asyncio import sleep
+from argparse import ArgumentParser
 
-import sys, traceback
+from pathlib import Path
+import os
 
-BOT_PREFIX = ('++')
+parser = ArgumentParser()
+parser.add_argument('-v', "--verbose", required=False, help="Run Iris.py with verbosity")
+
+args = parser.parse_args()
+
+if args.verbose.lower() == "true":
+	args.verbose = True
+else:
+	args.verbose = None
+
+if args.verbose:
+	print ("Reading Token...")
+
+with open("TOKEN", 'r') as TokenObj:
+	TOKEN = TokenObj.read()
+
+BOT_PREFIX = ('++', 'i+')
 bot = commands.Bot(command_prefix=BOT_PREFIX)
 
-"""cogs_dir = " "
+# Set args to bot
+bot.verbose = args.verbose
 
-#Load Extensions
-if __name__ == '__main__':
-    for extension in [f.replace('.py', '') for f in listdir(cogs_dir) if isfile(join(cogs_dir, f))]:
-        try:
-            bot.load_extension(cogs_dir + "." + extension)
-        except:
-            print ("FAIL#3 bot.load_extension(...)")
-            traceback.print_exc()
-"""
-@bot.command(name="testo", hidden=True)
-async def testo(ctx,  *args):
-    print ("Testing Function")
-
-"""OnReady_________"""
 @bot.event
 async def on_ready():
-    print ("\nLogged in as:\t" + str(bot.user.name))
-    print ("-----------------")
+	print ("\nLogged in as:\t" + str(bot))
+	print ("-----------------")
 
-    try:
-        await bot.change_presence(activity=discord.Game(name="my masters chores"))
-    except:
-        print ("FAIL#MS02 bot.change_presence(...)")
+	await bot.change_presence(activity=discord.Game(name="The Game of Life"))
 
-try:
-    bot.run(TOKEN)
-except:
-    print ("FAIL#MS01 bot.run(TOKEN)")
+if __name__ == '__main__':
+
+	# Load Extensions
+	cogs_dir = Path("core/cogs/")
+	cogs_list = []
+
+	for root, directories, files in os.walk(cogs_dir):
+		for fileName in files:
+			if '-cog' in fileName.lower() and fileName.endswith('.py'):
+				cogs_list.append(str(os.path.join(root, fileName )))
+
+	updated_cogs_list = []
+
+	for cog in cogs_list:
+		cog_new = str(cog)
+
+		# Windows
+		cog_new = cog_new.replace('core\\', '')
+		cog_new = cog_new.replace('\\', '.')
+
+		# Linux/Mac
+		cog_new = cog_new.replace('core/', '')
+		cog_new = cog_new.replace('/', '.')
+
+		cog_new = cog_new.replace(".py", "")
+
+		updated_cogs_list.append(cog_new)
+
+	cogs_list = updated_cogs_list
+
+	for cog in cogs_list:
+		print ("Loading Cog from Iris:\t", cog)
+
+		bot.load_extension(cog)
+
+
+	# RUN THE BOT
+	bot.run(TOKEN)
+
+	# Nothing should come after this. It will not be processed
+	# This comment won't even be encountered, r/funnyandsad :/
